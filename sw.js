f7dfedb6578a833c03790f93eec4be26a3c113df
mohamadsahafiz-spec <mohamadsahafiz@gmail.com>
2026-07-18 23:17:01 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+// This empty fetch handler is the "magic key" that tricks Chrome 
+// into recognizing the app as a fully installable PWA.
+self.addEventListener('fetch', function(event) {
+    // Leave this blank for now
+});
